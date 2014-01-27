d1f7025d152533f49fd5167eacb54a5785e991ab
Dario Sestero <dsestero@gmail.com>
2014-01-27 12:00:18 +0100

    corretto valori attributi hasstatus che dev'essere false e specificato pattern in modo tale che puppet riesca a gestire correttamente il servizio libreofficed che lancia un eseguibile con nome differente (soffice.bin)

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,9 +3,10 @@
 # Sets up a service for an headless LibreOffice.
 # It is intended to be called by libreoffice::libreoffice.
 class libreoffice::service {
-
   service { 'libreofficed':
     ensure     => running,
+    pattern    => "soffice.bin",
+    hasstatus  => false,
     hasrestart => false,
     enable     => true,
   }
